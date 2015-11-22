94ba1a8883cfac40105f5a337ecf952016dcbe02
J. Cohen <hostelmarketing@gmail.com>
2015-11-22 10:02:02 -0800

    Insert uploaded files into db'

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,7 +1,30 @@
 Template.profile.events({
     'submit .edit-profile': function (e) {
         var file = $('#profileImage').get(0).files[0];
-        console.log(file);
+
+        if (file) {
+            fsFile = new FS.File(file);
+            console.log('fsFile', fsFile);
+
+            ProfileImages.insert(fsFile, function (err, result) {
+                if (err) {
+                    throw new Meteor.Error(err);
+                } else {
+                    var imageLoc = '/cfs/files/ProfileImages/' + result._id;
+
+                    var insertObj = {
+                        userId: Meteor.userId(),
+                        username: Meteor.user().username,
+                        image: imageLoc
+                    };
+                    console.log('file', file);
+                    console.log('insertObj', insertObj);
+                    UserImages.insert(insertObj);
+
+                    Router.go('/');
+                }
+            });
+        }
         return false;
     }
 })
